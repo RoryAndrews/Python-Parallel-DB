b78dcc8d9c0b1292e6c590ee70716fee26daac95
Unknown <jeg6@hawaii.edu>
2017-03-21 00:08:13 -1000

    Added to SQLLoader
    Completed simple expression parser for WHERE clause. Returns expression as tuple (predicate, relation, predicate).

--- lib/SQLLoader.py
+++ lib/SQLLoader.py
@@ -25,15 +25,39 @@ class SQLLoader(MySQLListener):
                 return child.getText()
 
     def getExpression(self, ctx):
-        # first child will have 2 or more children
-        # 1st child is WHERE, 2nd child is expression, every other child in addition is an expression
-        # The next generation is the bool primary, this one should have three children, predicate, relational_op, and preidcate
-        
-        return ctx.getChild(1).getChild(0).getChild(2).getText()
+        temp_table1 = None
+        temp_table2 = None
+        temp_col1 = None
+        temp_col2 = None
+        temp_relation = None
+        expression = list()
 
+        if ctx.getChildCount() == 0:
+            return None
+        # if the context has a where clause it will have at least two children
+        #1ST GEN: 1st child is WHERE, 2nd child is expression, every other child in addition is an expression
+        #2ND GEN: The next generation is the bool primary, this one should have three children, predicate, relational_op, and preidcate
+        # If the predicate is simple, then you can stop at this level, if it is not continue
+        #8TH GEN: For a complex predicate you must go down to the 8th gen to get the escape clause
+        else:
+            for i in range(1,ctx.getChildCount(),2):
+                for j in range (0, ctx.getChild(i).getChild(0).getChildCount(),2):
+                    if temp_relation == None:
+                        temp_relation = ctx.getChild(i).getChild(0).getChild((j+1)).getText()
+                    if temp_col1 == None:
+                        temp_col1 = ctx.getChild(i).getChild(0).getChild(j).getText()
+                    else:
+                        temp_col2 = ctx.getChild(i).getChild(0).getChild(j).getText()
+                    if temp_col1 != None and temp_col2 != None:
+                        expression.insert(-1, (temp_col1, temp_relation, temp_col2))
+                        temp_col1 = None
+                        temp_col2 = None
+                        temp_relation = None
+        # getChild(0).getChild(0).getChild(0).getChild(0).getChild(0).getChild(0)
         # for i in range(1,ctx.getChildCount(),2):
         #
         # return ctx.getChild(1).getChild(0).getChildCount()
+        return expression
 
     def getSelect(self,ctx):
         self.select.insert(-1, (ctx.getChild(0).getChild(0).getText(), ctx.getChild(0).getChild(2).getText()))
