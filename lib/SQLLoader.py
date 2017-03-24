8a985a51aa610e206181d90350bc1500b67fe3fb
Unknown <jeg6@hawaii.edu>
2017-03-24 00:30:23 -1000

    SQLLoader
    gets full WHERE text

--- lib/SQLLoader.py
+++ lib/SQLLoader.py
@@ -26,37 +26,38 @@ class SQLLoader(MySQLListener):
         temp_col2 = None
         temp_relation = None
         expression = list()
-
-        if ctx.getChildCount() == 0:
-            return None
-        # if the context has a where clause it will have at least two children
-        #1ST GEN: 1st child is WHERE, 2nd child is expression, every other child in addition is an expression
-        #2ND GEN: The next generation is the bool primary, this one should have three children, predicate, relational_op, and preidcate
-        # If the predicate is simple, then you can stop at this level, if it is not continue
-        #8TH GEN: For a complex predicate you must go down to the 8th gen to get the escape clause
-        else:
-            for i in range(1,ctx.getChildCount(),2):
-                gen1 = self.visitChild(ctx,i)
-                for j in range (0, gen1.getChild(0).getChildCount(),2):
-                    gen2 = gen1.getChild(0)
-                    # print (type(gen2).__name__)
-                    if temp_relation == None:
-                        temp_relation = self.visitChild(gen2,j+1).getText()
-                    if temp_col1 == None:
-                        temp_col1 = self.visitChild(gen2,j).getText()
-                    else:
-                        temp_col2 = self.visitChild(gen2,j).getText()
-
-                    if temp_col1 != None and temp_col2 != None:
-                        expression.insert(-1, (temp_col1, temp_relation, temp_col2))
-                        temp_col1 = None
-                        temp_col2 = None
-                        temp_relation = None
-
-        # getChild(0).getChild(0).getChild(0).getChild(0).getChild(0).getChild(0)
-        # for i in range(1,ctx.getChildCount(),2):
+        expression = ctx.getText()
+        # if ctx.getChildCount() == 0:
+        #     return None
+        # # if the context has a where clause it will have at least two children
+        # #1ST GEN: 1st child is WHERE, 2nd child is expression, every other child in addition is an expression
+        # #2ND GEN: The next generation is the bool primary, this one should have three children, predicate, relational_op, and preidcate
+        # # If the predicate is simple, then you can stop at this level, if it is not continue
+        # #8TH GEN: For a complex predicate you must go down to the 8th gen to get the escape clause
+        # else:
+        #     expression = ctx.getText()
+        #     # for i in range(1,ctx.getChildCount(),2):
+        #     #     gen1 = self.visitChild(ctx,i)
+        #     #     for j in range (0, gen1.getChild(0).getChildCount(),2):
+        #     #         gen2 = gen1.getChild(0)
+        #     #         # print (type(gen2).__name__)
+        #     #         if temp_relation == None:
+        #     #             temp_relation = self.visitChild(gen2,j+1).getText()
+        #     #         if temp_col1 == None:
+        #     #             temp_col1 = self.visitChild(gen2,j).getText()
+        #     #         else:
+        #     #             temp_col2 = self.visitChild(gen2,j).getText()
+        #     #
+        #     #         if temp_col1 != None and temp_col2 != None:
+        #     #             expression.insert(-1, (temp_col1, temp_relation, temp_col2))
+        #     #             temp_col1 = None
+        #     #             temp_col2 = None
+        #     #             temp_relation = None
         #
-        # return ctx.getChild(1).getChild(0).getChildCount()
+        # # getChild(0).getChild(0).getChild(0).getChild(0).getChild(0).getChild(0)
+        # # for i in range(1,ctx.getChildCount(),2):
+        # #
+        # # return ctx.getChild(1).getChild(0).getChildCount()
         return expression
 
     def visitChild(self, ctx, branch):
