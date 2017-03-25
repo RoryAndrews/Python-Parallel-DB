d685cad9c2c0806bb26b30c3c515d3db3bd1c333
Unknown <jeg6@hawaii.edu>
2017-03-24 21:35:27 -1000

    Testcase 4
    Finished testcase4

--- Testcases/test4/test4-jeg6-1.sh
+++ Testcases/test4/test4-jeg6-1.sh
@@ -3,10 +3,12 @@ python3 runSQL.py test4-jeg6-1-load.cfg test4-jeg6-1.sql
 python3 runSQL.py test4-jeg6-1-load.cfg test4-jeg6-1-2.sql
 
 python3 runSQL.py test4-jeg6-1.cfg orders-test-data.csv | sort > test4-jeg6-1.out
-python3 runSQL.py test4-jeg6-1.cfg customers-test-data.csv | sort > test4-jeg6-1-2.out
+python3 runSQL.py test4-jeg6-1-2.cfg customers-test-data.csv | sort > test4-jeg6-1-2.out
+python3 runSQL.py test4-jeg6-1-catalog.cfg test4-jeg6-1.post.1.sql | sort > test4-jeg6-1-join.out
 
 ./test4-jeg6-1.post | sort > test4-jeg6-1.post.out
 diff -b ./test4-jeg6-1.post.out ./test4-jeg6-1.post.exp
+
 #Cleanup
 python3 runSQL.py test4-jeg6-1-load.cfg test4-jeg6-1.pre.sql
 python3 runSQL.py test4-jeg6-1-load.cfg test4-jeg6-1.pre-2.sql
